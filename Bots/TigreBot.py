7e024b502c0d3bc9c9a05192b5a76c2e0601e0f2
g-ae <goncalo.arieira@hotmail.com>
2025-12-15 11:26:34 +0100

    added minmax and integrated to TigreBot

--- Bots/TigreBot.py
+++ Bots/TigreBot.py
@@ -1,7 +1,18 @@
 from Bots.ChessBotList import register_chess_bot
+from Bots.RandomBot import *
 
 def chess_bot(player_sequence, board, time_budget, **kwargs):
-    # todo
-    return (0,0), (0,0)
+    b = [
+      ["","","","pw","pw","pw","",""],
+      ["","","","","","","",""],
+      ["","","","","","","",""],
+      ["","","","","","","",""],
+      ["","","","","","","",""],
+      ["","","","","","","",""],
+      ["","","","","","","",""],
+      ["","","","","","","",""]
+    ]
+    loaded_board = load_from_string(board)
+    return find_best_move(loaded_board, Pieces.white if player_sequence[1] == 'w' else Pieces.black, 4)
 
 register_chess_bot("TigreBot", chess_bot)
